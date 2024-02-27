ee6dd35b78702202008508b193daa715faff477a
parshucodes <parshuram14234@gmail.com>
2024-02-28 00:08:03 +0530

    testobject

--- Program.cs
+++ Program.cs
@@ -6,10 +6,10 @@ class Program
     static void Main(string[] args)
     {
         PatientRepository patientRepository = new PatientRepository();
-        //patientRepository.CreatePatient("parshuRam","M",24);
-        //patientRepository.CreateDoctor("Dr Chadda", "Orthopediac");
-        //DateTime times = new DateTime(2024,02,29,13,05,00);
-        //patientRepository.DoctorPatientAppointment_Create(3,3, times);
+        patientRepository.CreatePatient("parshuRam","M",24);
+        patientRepository.CreateDoctor("Dr Chadda", "Orthopediac");
+        DateTime times = new DateTime(2024,02,29,13,05,00);
+        patientRepository.DoctorPatientAppointment_Create(3,3, times);
         patientRepository.Patient_Update(25, "Mobile", 05, "O");
     }
 }
